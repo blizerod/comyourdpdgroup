eab774dadc1d8d9d78cb92e2647263cb6fa105d3
Oleksandr <blizerod@gmail.com>
2019-08-01 00:13:50 +0300

    initial commit

---
+++ src/test/java/SomeTest.java
@@ -0,0 +1,13 @@
+import org.testng.annotations.Test;
+
+import java.net.Socket;
+
+public class SomeTest {
+
+    @Test
+    public void someTest(){
+        System.out.println("TestNG");
+        System.out.println("fdg " + Integer.bitCount(122));
+    }
+
+}
